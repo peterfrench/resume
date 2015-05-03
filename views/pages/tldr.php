fe9e0cd2d22eca239eb8a89078dd8a17cb7a22e9
Peter French <peterfrench.b@gmail.com>
2015-05-03 06:26:00 -0500

    added hard copy of resume

--- views/pages/tldr.php
+++ views/pages/tldr.php
@@ -2,7 +2,7 @@
 <div class="container">
 <div class="jumbotron well">
 	<h2>TL;DR?</h2>
-	<p>Here's a condensed &amp; simplified pdf version of Peter's <a href="">resume</a>.</p>
+	<p>Here's a condensed &amp; simplified pdf version of Peter's <a href="/documents/resume.pdf">resume</a>.</p>
 	<blockquote>
 	  <p><i class="fa fa-quote-left"></i> What did I just read? <i class="fa fa-quote-right"></i></p>
 	  <footer>You</footer>
