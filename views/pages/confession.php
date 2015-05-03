f50771cee833a03bebb5450dcfae957a7b7eb9a6
Peter French <peterfrench.b@gmail.com>
2015-05-03 06:26:17 -0500

    better starting point for video

--- views/pages/confession.php
+++ views/pages/confession.php
@@ -2,7 +2,7 @@
 <div class="container">
 <div class="jumbotron well">
 	<h2>Wait, those are all the same person...</h2>
-	<p>Yes, yes they are. How can Peter do all those roles? Is he <a href="https://www.youtube.com/watch?v=bd2B6SjMh_w&feature=youtu.be&t=43s" target="_blank">crazy</a>? Maybe. But mostly it's because he just loves to build sweet software.</p>
+	<p>Yes, yes they are. How can Peter do all those roles? Is he <a href="https://www.youtube.com/watch?v=bd2B6SjMh_w&feature=youtu.be&t=35s" target="_blank">crazy</a>? Maybe. But mostly it's because he just loves to build sweet software.</p>
 	<blockquote class="blockquote-reverse">
 		<p><i class="fa fa-quote-left"></i> It's taken a great amount of time and effort to get where I am now. I have had the pleasure of working with some of the most knowledgable professionals in the industry. They have all taught me so much and I am very grateful for them. <i class="fa fa-quote-right"></i></p>
 		<footer>Peter French</footer>
