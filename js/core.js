7b45a5f413e3eb705a971f302fe20c8304c426d7
Peter French <peterfrench.b@gmail.com>
2015-05-03 22:48:59 -0500

    Fixed android google chrome issue. - When scrolling up, the address bar would display & change the height of the window. This would change the height of each page and jump the user to a different scroll position on the site.

--- js/core.js
+++ js/core.js
@@ -4,12 +4,23 @@
 $('[data-toggle="tooltip"]').tooltip();
 
 // Animate start button
-$('#start').on('click', function(){
+$('#start').bind('click', function(){
 	var anchor = $(this).attr('href');
 	$('html, body').animate({
 		scrollTop: $(anchor).offset().top
 	}, 500);
 	return false;
+});
+
+// On load, set page height to fix issue with google chrome on android.
+// When the browser's address bar comes back into view, it changes the value of the window height.
+var setPageHeights = function() {
+	$('.page').css({ height: $(this).height()+'px'});
+}
+setPageHeights();
+
+$(window).bind('resize',function(){
+	setPageHeights();
 });
 
 })})(jQuery);
