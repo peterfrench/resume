29680f69022a496290d7680f303bb70cf4a0ebe8
Peter French <peterfrench.b@gmail.com>
2015-05-07 18:13:03 -0500

    wording tweak

--- views/pages/roleDesigner.php
+++ views/pages/roleDesigner.php
@@ -6,7 +6,7 @@
 	  <p><i class="fa fa-quote-left"></i> Comic Sans is not in <i>my</i> font family. <i class="fa fa-quote-right"></i></p>
 	</blockquote>
 	<p>He strives for perfection. A good user interface can make or break an app. He knows this and he likes to talk about it <i>all the time</i>. Believe me.</p>
-	<p>Whether you are on a Mobile, Tablet, or Desktop device, he has you covered. (Even phablets.)</p>
+	<p>Whether you are on a Mobile, Tablet, or Desktop device, he has you covered. (Oh yes, even phablets.)</p>
 	<blockquote class="blockquote-reverse">
 	  <p><i class="fa fa-quote-left"></i> That's, like, 2 pixels off. <i class="fa fa-quote-right"></i></p>
 	</blockquote>
